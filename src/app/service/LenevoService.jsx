87c22a5d1fcdf0d3ad66c3497b5972e4ba5b60b2
jitendra diwakar <jitendra.diwaker@gmail.com>
2018-06-25 16:42:25 +0530

    Changes done in Lenovo page

--- src/app/service/LenevoService.jsx
+++ src/app/service/LenevoService.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 export class LenevoService extends React.Component {
     render() {
         return (
-            <div>
-                <p>
-                    <strong>Axis Computer</strong> provides the best service in all kind of Laptops like Lenovo,
-                we provide all kind of service for <strong>Lenovo Laptop </strong> like Screen Replacement, MotherBoard
-                    Replacement, Software Installation, Windows Installation, KeyBoard Replacement and many more.
-              </p>
+            <section id="services">
+            <div className="container">
+  
+                <header className="section-header">
+                    <h3>Sony Service center</h3>
+                    <p>
+                        <strong>Axis Computer</strong> provides the best service in all kind of Laptops like Lenovo,
+                            we provide all kind of service for <strong>Lenovo Laptop </strong> like Screen Replacement, MotherBoard
+                        Replacement, Software Installation, Windows Installation, KeyBoard Replacement and many more.
+                    </p>
+                </header>
             </div>
+        </section>
         );
     }
 }
