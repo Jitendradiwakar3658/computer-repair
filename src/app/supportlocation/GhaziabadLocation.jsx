ac67955a296948aea00b6e178805f98be7af4443
unknown <jitendra.diwaker@gmail.com>
2018-07-01 16:36:21 +0530

    Added three pages in SupportLocation

---
+++ src/app/supportlocation/GhaziabadLocation.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+export class GhaziabadLocation extends React.Component {
+    render() {
+        return (
+            <div>
+                <section id="services">
+                    <div className="container"> <header className="section-header">
+                        <h3>Ghaziabad Location</h3>
+                        <p>
+                            <strong>Axis Computer</strong> provides the best service in all kind of Laptops like Hp,
+                                    we provide all kind of service for <strong>Hp Laptop </strong> like Screen Replacement, MotherBoard
+                        Replacement, Software Installation, Windows Installation, KeyBoard Replacement and many more.
+                        </p>
+                    </header>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+export default GhaziabadLocation;
